27402816d683e6e3153aad6f7962a9d6da6b230a
Kokitis <sypilus@gmail.com>
2017-06-06 21:25:13 -0400

    Converted indentation back to tabs

--- filetools.py
+++ filetools.py
@@ -5,131 +5,131 @@ from pprint import pprint
 
 
 def checkDir(path, full = False):
-    """ Creates a folder if it doesn't already exist.
-        Parameters
-        ----------
-            path: string
-                Path to a folder.
-            full: bool; default False
-                If true, will create any parent folders
-                if they do not already exists.
-        Returns
-        -------
-            status: bool
-                Whether the folder was successfully created.
-    """
+	""" Creates a folder if it doesn't already exist.
+		Parameters
+		----------
+			path: string
+				Path to a folder.
+			full: bool; default False
+				If true, will create any parent folders
+				if they do not already exists.
+		Returns
+		-------
+			status: bool
+				Whether the folder was successfully created.
+	"""
 
-    if not os.path.exists(path):
-        if full:
-            os.makedirs(path)
-        else:
-            os.mkdir(path)
+	if not os.path.exists(path):
+		if full:
+			os.makedirs(path)
+		else:
+			os.mkdir(path)
 
-    return os.path.exists(path)
+	return os.path.exists(path)
 
 
 def generateFileMd5(filename, blocksize=2**20):
-    """ Generates the md5sum of a file. Does
-        not require a lot of memory.
-        Parameters
-        ----------
-            filename: string
-                The file to generate the md5sum for.
-            blocksize: int; default 2**20
-                The amount of memory to use when
-                generating the md5sum string.
-        Returns
-        -------
-            md5sum: string
-                The md5sum string.
-    """
-    m = hashlib.md5()
-    with open(filename, "rb") as f:
-        while True:
-            buf = f.read(blocksize)
-            if not buf: break
-            m.update(buf)
-    return m.hexdigest()
+	""" Generates the md5sum of a file. Does
+		not require a lot of memory.
+		Parameters
+		----------
+			filename: string
+				The file to generate the md5sum for.
+			blocksize: int; default 2**20
+				The amount of memory to use when
+				generating the md5sum string.
+		Returns
+		-------
+			md5sum: string
+				The md5sum string.
+	"""
+	m = hashlib.md5()
+	with open(filename, "rb") as f:
+		while True:
+			buf = f.read(blocksize)
+			if not buf: break
+			m.update(buf)
+	return m.hexdigest()
 
 
 def listAllFiles(folder, **kwargs):
-    """ Lists all files in a folder. Includes subfolders.
-        Parameters
-        ----------
-            folder: string
-                The folder to search through.
-        Keyword Arguments
-        -----------------
-            exclude: string, list<string>; default []
-                A path or list of paths to exclude from the recursive search.
-            logic: {'or', 'and'}; default 'or'
-                The logic to use when applying the exclusion criteria.
-        Returns
-        -------
-            list<string>
-                A list of all files that were found.
-    """
-    exclude = kwargs.get('exclude', [])
-    if isinstance(exclude, str): exclude = [exclude]
-    logic = kwargs.get('logic', 'or')
-    
-    file_list = list()
+	""" Lists all files in a folder. Includes subfolders.
+		Parameters
+		----------
+			folder: string
+				The folder to search through.
+		Keyword Arguments
+		-----------------
+			exclude: string, list<string>; default []
+				A path or list of paths to exclude from the recursive search.
+			logic: {'or', 'and'}; default 'or'
+				The logic to use when applying the exclusion criteria.
+		Returns
+		-------
+			list<string>
+				A list of all files that were found.
+	"""
+	exclude = kwargs.get('exclude', [])
+	if isinstance(exclude, str): exclude = [exclude]
+	logic = kwargs.get('logic', 'or')
+	
+	file_list = list()
 
-    for fn in os.listdir(folder):
-        abs_path = os.path.join(folder, fn)
-        if logic == 'or':
-            skip_file = any(e in abs_path for e in exclude)
-        elif logic == 'and':
-            skip_file = all(e in abs_path for e in exclude)
-        else:
-            skip_file = False
+	for fn in os.listdir(folder):
+		abs_path = os.path.join(folder, fn)
+		if logic == 'or':
+			skip_file = any(e in abs_path for e in exclude)
+		elif logic == 'and':
+			skip_file = all(e in abs_path for e in exclude)
+		else:
+			skip_file = False
 
-        if skip_file: continue
-        if os.path.isdir(abs_path):
-            file_list += listAllFiles(abs_path, **kwargs)
-        elif os.path.isfile(abs_path):  # Explicit check
-            file_list.append(abs_path)
-    return file_list
+		if skip_file: continue
+		if os.path.isdir(abs_path):
+			file_list += listAllFiles(abs_path, **kwargs)
+		elif os.path.isfile(abs_path):  # Explicit check
+			file_list.append(abs_path)
+	return file_list
 
 
 def searchForDuplicateFiles(folder, by = 'name'):
-    """ Searches for duplicate files in a folder.
-        Parameters
-        ----------
-            folder: string [Path]
-                The folder to search through. subfolders will be included.
-            by: {'md5', 'name', 'size'}; default 'md5'
-                The method to qualify two files as being the same.
-        Return
-        ------
-            duplicates: list<list<string>>
-                A list of paired filenames representing identical files.
-    """
-    all_files = listAllFiles(folder)
+	""" Searches for duplicate files in a folder.
+		Parameters
+		----------
+			folder: string [Path]
+				The folder to search through. subfolders will be included.
+			by: {'md5', 'name', 'size'}; default 'md5'
+				The method to qualify two files as being the same.
+		Return
+		------
+			duplicates: list<list<string>>
+				A list of paired filenames representing identical files.
+	"""
+	all_files = listAllFiles(folder)
 
-    checked_files = dict()
-    _duplicate_keys = list()
-    for filename in all_files:
-        if by == 'md5':
-            file_key = generateFileMd5(filename)
-        elif by == 'name':
-            file_key = os.path.basename(filename)
-        elif by == 'size':
-            file_key = os.path.getsize(filename)
+	checked_files = dict()
+	_duplicate_keys = list()
+	for filename in all_files:
+		if by == 'md5':
+			file_key = generateFileMd5(filename)
+		elif by == 'name':
+			file_key = os.path.basename(filename)
+		elif by == 'size':
+			file_key = os.path.getsize(filename)
 
-        if file_key in checked_files:
-            checked_files[file_key].append(filename)
-            _duplicate_keys.append(file_key)
-        else:
-            checked_files[file_key] = [filename]
-    _duplicates = list()
-    for key in _duplicate_keys:
-        _duplicates.append(checked_files[key])
-    return _duplicates
+		if file_key in checked_files:
+			checked_files[file_key].append(filename)
+			_duplicate_keys.append(file_key)
+		else:
+			checked_files[file_key] = [filename]
+	_duplicates = list()
+	for key in _duplicate_keys:
+		_duplicates.append(checked_files[key])
+	return _duplicates
 
 
 if __name__ == "__main__":
-    test_folder = "/home/upmc/Documents/Genomic_Analysis/1_input_vcfs/original_callsets/TCGA-2H-A9GF"
-    test_exclude = ['chromosome']
-    result = listAllFiles(test_folder, exclude = test_exclude, logic = 'and')
-    pprint(result)
+	test_folder = "/home/upmc/Documents/Genomic_Analysis/1_input_vcfs/original_callsets/TCGA-2H-A9GF"
+	test_exclude = ['chromosome']
+	result = listAllFiles(test_folder, exclude = test_exclude, logic = 'and')
+	pprint(result)
--- maptools.py
+++ maptools.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    filename = ""
-    shapepath = geopandas.datasets.get_path("naturalearth_lowres")
-    shape = geopandas.read_file(shapepath)
-    shape.plot()
-    plt.show()
-    print(shape)
+	filename = ""
+	shapepath = geopandas.datasets.get_path("naturalearth_lowres")
+	shape = geopandas.read_file(shapepath)
+	shape.plot()
+	plt.show()
+	print(shape)
