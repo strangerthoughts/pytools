1b45fce64655e1dd6521f94603c65b2a5863b0f2
Kokitis <sypilus@gmail.com>
2017-06-19 16:36:48 -0400

    Rewrote Terminal() as a class

--- systemtools.py
+++ systemtools.py
@@ -24,73 +24,78 @@ def memoryUsage(show = True, units = 'MB'):
 	else:
 		return usage
 
+
+class Terminal1:
+	""" Wrapper around python system modules.
+		Makes it easy to run a command and save any output.
+	"""
+	def __init__(self, command, label = "", filename = None, show_output = False, use_system = False):
+		self.label = label
+
+		terminal_label = self._getCommandLabel(label)
+		self.output = self._runCommand(command, terminal_label, use_system)
+		if show_output:
+			print(self.output)
+		if filename is not None:
+			self.updateConsoleLog(filename, command, self.output, label)
+
+	@staticmethod
+	def _getCommandLabel(label):
+		if label == "": return label
+		char = "#"
+		max_len = 180
+		edge = char * max_len
+		edgelen = int(max_len/2 - int(len(label) / 2))
+		middle = edgelen * char + label + edgelen*char
+
+		return "\n".join([edge, middle, edge])
+
+	@staticmethod
+	def _runCommand(command, label, use_system = False):
+		if label != "":
+			print(label)
+		if use_system:
+			output = os.system(command)
+		else:
+			command = shlex.split(command)
+			process = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+			output = str(process.stdout.read(), 'utf-8')
+		return output
+
+	@staticmethod
+	def updateConsoleLog(filename, command, output, label = ""):
+		""" Saves any text printed to the console to a file.
+			Parameters
+			----------
+			filename: string
+				The file to save the text to.
+			command: string
+				The command that was executed.
+			output: string
+				The text that was printed to the terminal. Saved
+				from process.stdout.read()
+			label: string
+				A string to write prior to writing the
+				relevant text. Used to distinguish between
+				different command that were executed.
+		"""
+
+		if os.path.exists(filename): opentype = 'a'
+		else: opentype = 'w'
+		with open(filename, opentype) as console_file:
+			console_file.write(label + '\n')
+			console_file.write(' '.join(command) + '\n')
+			console_file.write(output + '\n\n')
+
+	def run(self, command, label, filename):
+		pass
+
 class Terminal:
-    """ Wrapper around python system modules.
-        Makes it easy to run a command and save any output.
-    """
-    def __init__(self, command, label = "", filename = None, show_output = False, use_system = False):
-        self.label = label
-
-        terminal_label = self._getCommandLabel(label)
-        self.output = self._runCommand(command, terminal_label, use_system)
-        if show_output:
-            print(self.output)
-        if filename is not None:
-            self.updateConsoleLog(filename, command, self.output, label)
-
-    @staticmethod
-    def _getCommandLabel(label):
-        if label == "": return label
-        char = "#"
-        max_len = 180
-        edge = char * max_len
-        edgelen = int(max_len/2 - int(len(label) / 2))
-        middle = edgelen * char + label + edgelen*char
-
-        return "\n".join([edge, middle, edge])
-
-    @staticmethod
-    def _runCommand(command, label, use_system = False):
-        if label != "":
-            print(label)
-        if use_system:
-            output = os.system(command)
-        else:
-            command = shlex.split(command)
-            process = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-            output = str(process.stdout.read(), 'utf-8')
-        return output
-
-    @staticmethod
-    def updateConsoleLog(filename, command, output, label = ""):
-        """ Saves any text printed to the console to a file.
-            Parameters
-            ----------
-            filename: string
-                The file to save the text to.
-            command: string
-                The command that was executed.
-            output: string
-                The text that was printed to the terminal. Saved
-                from process.stdout.read()
-            label: string
-                A string to write prior to writing the
-                relevant text. Used to distinguish between
-                different command that were executed.
-        """
-
-        if os.path.exists(filename): opentype = 'a'
-        else: opentype = 'w'
-        with open(filename, opentype) as console_file:
-            console_file.write(label + '\n')
-            console_file.write(' '.join(command) + '\n')
-            console_file.write(output + '\n\n')
-
-    def run(self, command, label, filename):
-        pass
+	def __init__(self, command, label = "", expected_output = None, filename = None, show_output = False):
 		self.command = command
 		self.label = label
 		self.filename = filename
+		self.show_output = show_output
 
 		if expected_output is None:
 			self.expected_output = []
@@ -103,14 +108,18 @@ class Terminal:
 		self.runCommand()
 
 	def runCommand(self):
+		command_arguments = shlex.split(self.command)
 		if any(not os.path.exists(fn) for fn in self.expected_output):
+
 			#self._printCommand(command_arguments)
 			process = subprocess.Popen(command_arguments, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
 			self.output = str(process.stdout.read(), 'utf-8')
 		else:
 			self.output = ""
 
+		self._showOutput(command_arguments)
 
+	def _showOutput(self, command_arguments):
 
 		command_string = "\tCommand Arguements:\n"
 		for argument in command_arguments:
@@ -123,6 +132,7 @@ class Terminal:
 		if len(self.expected_output) > 0:
 			expected_output_string = "\tExpected Output: \n"
 			for eo in self.expected_output:
+				line = "\t\t" + eo + "\n"
 				expected_output_string += line
 		else:
 			expected_output_string = ""
