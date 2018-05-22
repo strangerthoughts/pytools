99f541d8c4d6298e0af442e1abd8f4426ecc9d0a
Unknown <sypilus@gmail.com>
2018-05-22 01:04:53 -0400

    Updated tests to support pathlib

--- tests/tabletools_tests.py
+++ tests/tabletools_tests.py
@@ -2,6 +2,7 @@ from pytools import tabletools
 
 import unittest
 import os
+from pathlib import Path
 DATA_FOLDER = os.path.join(os.path.dirname(__file__), 'test_data')
 basename = os.path.join(DATA_FOLDER, 'Annual State Populations')
 class TestDatabase(unittest.TestCase):
@@ -21,6 +22,10 @@ class TestDatabase(unittest.TestCase):
 		pass
 	def test_read_xlsx_with_kwargs(self):
 		pass
+
+	def test_read_from_pathlib(self):
+		filename = Path(basename).with_suffix('.xlsx')
+		table = tabletools.Table(filename)
 
 if __name__ == "__main__":
 	unittest.main()
